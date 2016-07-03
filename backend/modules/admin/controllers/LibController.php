83fbc104617aefb5215283c223c652417d8290ff
mr.p. khamperngjai <superpck@yahoo.com>
2016-07-03 10:53:42 +0700

    update lib controller

--- backend/modules/admin/controllers/LibController.php
+++ backend/modules/admin/controllers/LibController.php
@@ -16,13 +16,28 @@ class LibController extends Controller
     public function actionHospitals($q = null)
     {
         $out = [];
-        if (mb_strlen($q,'UTF-8') > 1){
-            $Sql = 'SELECT * FROM admin.lib_hospcode WHERE name LIKE "%' . $q .'%" '
+        if (mb_strlen($q,'UTF-8') > 2){
+            $Sql = 'SELECT * FROM lib_hospcode WHERE name LIKE "%' . $q .'%" '
                     . ' OR off_id LIKE "%' . $q .'%" '
                     . ' ORDER BY name,off_id LIMIT 20 ';
             $data = Yii::$app->db_admin->createCommand($Sql)->queryAll();
             foreach ($data as $value) {
                 $out[] = ['value' => $value['off_id'].' , '.$value['name'].' , prov:'.$value['changwat'].' '.$value['typename']];
+            }
+        }
+        echo Json::encode($out);
+    }
+
+    public function actionChospitals($q = null)
+    {
+        $out = [];
+        if (mb_strlen($q,'UTF-8') > 2){
+            $Sql = 'SELECT * FROM chospital WHERE hosname LIKE "%' . $q .'%" '
+                    . ' OR hoscode LIKE "%' . $q .'%" '
+                    . ' ORDER BY hosname LIMIT 20 ';
+            $data = Yii::$app->db_nrefer_hdc->createCommand($Sql)->queryAll();
+            foreach ($data as $value) {
+                $out[] = ['value' => $value['hoscode'].' , '.$value['hosname'].' , prov:'.$value['provcode'].' Dept:'.$value['dep']];
             }
         }
         echo Json::encode($out);
