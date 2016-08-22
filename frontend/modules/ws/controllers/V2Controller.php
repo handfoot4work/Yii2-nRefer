ff7b8237fede2b505d50a61425f87d64b89894bf
mr.p. khamperngjai <superpck@yahoo.com>
2016-08-22 17:25:37 +0700

    update ws

--- frontend/modules/ws/controllers/V2Controller.php
+++ frontend/modules/ws/controllers/V2Controller.php
@@ -26,6 +26,8 @@ class V2Controller extends Controller
                 'actions' => [
                     'post' => ['post'],
                     'request' => ['post'],
+                    'get' => ['post'],
+                    'put' => ['put'],
                 ],
             ],
         ];
@@ -164,7 +166,7 @@ class V2Controller extends Controller
     {
         $getUser = $this->getUser($Post["api-key"],$Post["secret-key"]);
         if (sizeof($getUser)==0 || $getUser["api_key"]=="") {
-            return ['success'=>0,'error'=>400,'message_response'=>Yii::$app->params["statusCodes"][400].' (Incorrect api-key or secret-key-current)'];
+            return ['success'=>0,'error'=>400,'message_response'=>Yii::$app->params["statusCodes"][400].' (Incorrect api-key or secret-key)'];
         }
 
         $Token = md5($this->today.$Post["secret-key"]);
@@ -286,17 +288,12 @@ class V2Controller extends Controller
             return false;
         }
 
-        //$Posts["client"]["PHP_AUTH_DIGEST"] = $_SERVER["PHP_AUTH_DIGEST"];
-        //$Posts["client"]["PHP_AUTH_USER"] = $_SERVER["PHP_AUTH_USER"];
-        //$Posts["client"]["PHP_AUTH_PW"] = $_SERVER["PHP_AUTH_PW"];
-        //$Posts["client"]["AUTH_TYPE"] = $_SERVER["AUTH_TYPE"];
         $this->Agruments = $Posts;
 
         $TablesName = Yii::$app->db_nrefer_hdc->createCommand('SHOW tables')->queryAll();
         $hdcTables = $this->getHDCTable();
         $existTables = $this->getTable() ;
         $lcSql = [];
-
         foreach ($this->Data as $Table_Name=>$TableData){
             if (!array_search($Table_Name,$hdcTables) || !array_search($Table_Name,$existTables) ) {
                 return json_encode([
@@ -322,8 +319,10 @@ class V2Controller extends Controller
                 $Values = "";
                 foreach ($row as $fld=>$data){
                     $fldName = strtolower($fld);
-                    $Columns .= ($Columns==""? '':' , ').$TableDescription[$fldName]["name"]  ;
-                    $Values .= ($Values==""? ' "':' , "').$data.'"';
+                    if ($TableDescription[$fldName]["name"] !=""){
+                        $Columns .= ($Columns==""? '':' , ').$TableDescription[$fldName]["name"]  ;
+                        $Values .= ($Values==""? ' "':' , "').$data.'"';
+                    }
                     $TableDescription[$fldName]["null"] = false ;
                 }
                 $lcSql[] = 'REPLACE INTO '.Yii::$app->params["table_std_prefix"].$Table_Name.' ('.$Columns.') VALUES ('.$Values.')';
@@ -334,7 +333,8 @@ class V2Controller extends Controller
                 $ResultSQL[] = $IsFound;
             }
         }
-
+        //echo json_encode($lcSql);
+        //exit;
         $ResultSql = [];
         foreach ($lcSql as $key => $sql) {
             if ($ResultSQL[$key]==""){
@@ -402,14 +402,117 @@ class V2Controller extends Controller
     }
 
 // ========================================================================
-    public function actionGet($token=null,$type=send,$seq=null,$pid=null,$an=null,$cid=null,$prov=null,$lastupdate=null,$date=null)
+    private function read_data($param)
+// ========================================================================
+{
+    //if (!$this->ServerAvaliable($param) || !$this->checkToken($param["token"]) ){
+    //    exit ;
+    //}
+
+    if (!$this->checkToken($param["token"]) ){
+        exit ;
+    }
+    $return = [
+                'success'=>1,
+                'date_response'=>$this->today,
+                'keyw'=>$keyw,
+                'error'=>0,
+                'message_response' =>'' ,
+                'params'=>$param
+            ];
+    return json_encode($return);
+}
+
+// ========================================================================
+    public function actionData($token=null,$type=send,$seq=null,$pid=null,
+                                $an=null,$cid=null,$prov=null,$lastupdate=null,
+                                $date=null,$keyw=null)
+// ========================================================================
+    {
+        $this->today = date("Y_m_d H:i:s");
+        if ($token=="" || $cid=="" || ($seq=="" && $pid=="" && $an=="" && $prov=="" && $date=="" && $lastupdate=="" )) {
+            echo json_encode([
+                    'success'=>0,
+                    'date_response'=>$this->today,
+                    'keyw'=>$keyw,
+                    'error'=>400,
+                    'message_response' =>Yii::$app->params["statusCodes"][400]." (Not found any agrument for search data.)" ,
+                ]);
+            return false;
+        }
+        return $this->read_data([
+            'token'=>$token,
+            'type'=>$type,
+            'seq'=>$seq,
+            'pid'=>$pid,
+            'an'=>$an,
+            'cid'=>$cid,
+            'prov'=>$prov,
+            'date'=>$date,
+            'keyw'=>$keyw,
+        ]);
+    }
+
+// ========================================================================
+    public function actionGet()
 // ========================================================================
     {
         $headers = Yii::$app->response->headers;
         $headers->add('Content-Type', 'application/json');
         $headers->add('Access-Control-Allow-Origin', '*');
         //$headers->add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        //$headers->add('Access-Control-Allow-Origin:', $_SERVER['HTTP_ORIGIN']);
+        //$headers->add('ccess-Control-Allow-Methods', 'POST, GET, OPTIONS');
         $headers->add('Access-Control-Allow-Methods', 'POST');
+
+        $this->today = date("Y_m_d H:i:s");
+        $Post = (array) Yii::$app->request->post();
+        if ($Post["token"]=="" || ($Post["date"]=="" && $Post["cid"]=="") ) {
+            echo json_encode([
+                    'success'=>0,
+                    'date_response'=>date("Y-m-d H:i:s"),
+                    'error'=>400,
+                    'message_response' =>Yii::$app->params["statusCodes"][400]." (Not found any agrument for search data.)" ,
+                ]);
+            return false;
+        }
+        if (!$this->checkServerAvaliable() || !$this->checkToken($Post["token"]) ){
+            exit ;
+        }
+
+        if ($Post["date"] != ""){
+            return json_encode($this->GetByDate($Post));
+        } else if ($seq != ""){
+            return json_encode($this->GetBySeq($Post));
+        }
+
+        return json_encode([
+            'success'=>0,
+            'date_response'=>date("Y-m-d H:i:s"),
+            'post'=>$Post,
+            'error'=>404,
+            'message_response'=>Yii::$app->params["statusCodes"][404],
+        ]);
+    }
+
+// ========================================================================
+    public function actionGetHistory($token=null,$type=send,$seq=null,$pid=null,$an=null,$cid=null,$prov=null,$lastupdate=null,$date=null)
+// ========================================================================
+    {
+        $headers = Yii::$app->response->headers;
+        $headers->add('Content-Type', 'application/json');
+        $headers->add('Access-Control-Allow-Origin', '*');
+
+        echo date("Y-m-d H:i:s");
+
+        $Sql = "select * from person limit 1";
+        $referHistory= Yii::$app->db_nrefer_hdc->createCommand($Sql)->queryAll();
+        print_r($rawData);
+
+        $rawData = Yii::$app->db_hadoop->createCommand($Sql)->queryAll();
+        print_r($rawData);
+
+        exit;
 
         $this->today = date("Y_m_d H:i:s");
         $Post = (array) Yii::$app->request->post();
@@ -454,17 +557,14 @@ class V2Controller extends Controller
     }
 
 // ========================================================================
-    private function GetBySeq($token,$type,$seq)
+    private function GetBySeq($Post)
 // ========================================================================
     {
-        /*"prov": "40",
-          "region": "07",
-          "provider": "3", */
-        $Sql = 'SELECT * FROM view_refer_token WHERE token="'.$token.'" ';
+        $Sql = 'SELECT * FROM view_refer_token WHERE token="'.$Post["token"].'" ';
         $getToken = Yii::$app->db_admin->createCommand($Sql)->queryOne();
 
         $Sql = 'SELECT r.* FROM refer_history r LEFT JOIN chospital h on r.HOSPCODE=h.hoscode '
-                . ' WHERE SEQ="'.$seq.'" AND h.provcode="'.$getToken["prov"].'" ';
+                . ' WHERE SEQ="'.$Post["seq"].'" AND h.provcode in ('.$getToken["prov"].') ';
         $referHistory= Yii::$app->db_nrefer_hdc->createCommand($Sql)->queryAll();
         $pid = $referHistory[0]["PID"];
         $hospcode = $referHistory[0]["HOSPCODE"];
@@ -480,27 +580,34 @@ class V2Controller extends Controller
     }
 
     // ========================================================================
-        private function GetByDate($token,$type,$date)
+        private function GetByDate($Post)
     // ========================================================================
         {
-            /*"prov": "40",
-              "region": "07",
-              "provider": "3", */
-            $Sql = 'SELECT * FROM view_refer_token WHERE token="'.$token.'" ';
+            $Sql = 'SELECT * FROM view_refer_token WHERE token="'.$Post["token"].'" ';
             $getToken = Yii::$app->db_admin->createCommand($Sql)->queryOne();
 
-            if (strtolower($type)=='send'){
-                $Sql = 'SELECT r.* '
+            if (strtolower($Post["type"])=='send'){
+                $Sql = (isset($Post["individual"]) && $Post["individual"]+0==1? 'SELECT r.* ':'SELECT count(1) as cases ')
                         . ' FROM refer_history r LEFT JOIN chospital h on r.HOSPCODE=h.hoscode '
-                        . ' WHERE substr(r.DATETIME_REFER,1,10)="'.$date.'" '
-                        . ' AND h.provcode="'.$getToken["prov"].'" ';
+                        . ' WHERE substr(r.DATETIME_REFER,1,10)="'.$Post["date"].'" '
+                        . ' AND h.provcode in ('.$getToken["prov"].') ';
             } else {
-                $Sql = 'SELECT r.* '
+                $Sql = (isset($Post["individual"]) && $Post["individual"]+0==1? 'SELECT r.* ':'SELECT count(1) as cases ')
                         . ' FROM refer_history r LEFT JOIN chospital d on r.HOSP_DESTINATION=d.hoscode '
                         . ' LEFT JOIN chospital h on r.HOSPCODE=h.hoscode '
-                        . ' WHERE substr(r.DATETIME_REFER,1,10)="'.$date.'" AND r.HOSP_DESTINATION!="" '
-                        . ' AND d.provcode="'.$getToken["prov"].'" AND  h.provcode!="'.$getToken["prov"].'" ';
+                        . ' WHERE substr(r.DATETIME_REFER,1,10)="'.$Post["date"].'" AND r.HOSP_DESTINATION!="" '
+                        . ' AND d.provcode in ('.$getToken["provgroup"].')  ';
+                        //. ' AND d.provcode in ('.$getToken["provgroup"].') AND  h.provcode!="'.$getToken["prov"].'" ';
             }
+            if (isset($Post["cid"]) && $Post["cid"]!=""){
+                //$Sql .= ' AND r.CID ="'.str_replace([" ",";"],["",""],$Post["cid"]).'"';
+            }
+            if (isset($Post["limit"]) && $Post["limit"]+0>0 && isset($Post["start"]) ){
+                $Sql .= ' LIMIT '.($Post["start"]+0).' , '.($Post["limit"]+0);
+            } else {
+                $Sql .= ' LIMIT  0,10 ';
+            }
+
             $referHistory= Yii::$app->db_nrefer_hdc->createCommand($Sql)->queryAll();
             $pid = $referHistory[0]["PID"];
             $hospcode = $referHistory[0]["HOSPCODE"];
@@ -548,7 +655,7 @@ class V2Controller extends Controller
         $getToken = Yii::$app->db_admin->createCommand($Sql)->queryOne();
 
         if ($getToken["token"]=="") {
-            echo json_encode(['success'=>0,'date_response'=>$this->today,'error'=>400,'message_response'=>Yii::$app->params["statusCodes"][400].' (token not found)']);
+            echo json_encode(['success'=>0,'date_response'=>$this->today,'error'=>400,'message_response'=>Yii::$app->params["statusCodes"][400].' (token not valid)']);
             return false;
         }
         $Today = mktime(substr($this->today,11,2),substr($this->today,14,2),substr($this->today,17,2),substr($this->today,5,2),substr($this->today,8,2),substr($this->today,0,4) );
@@ -558,6 +665,22 @@ class V2Controller extends Controller
             return false;
         }
 
+        return true;
+    }
+
+// ========================================================================
+    private function checkServerAvaliable()
+// ========================================================================
+    {
+        if (Yii::$app->params["serverMaintenance"]){
+            echo json_encode([
+                'success'=>0,
+                'date_response'=>$this->today,
+                'error'=>501,
+                'message_response'=>Yii::$app->params["statusCodes"][501],
+            ]);
+            return false;
+        }
         return true;
     }
 
